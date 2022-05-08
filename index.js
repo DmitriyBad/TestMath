a2b0f2b482a020edb599d922d65baf9951aaa924
Dima <im.dmitriy.bovkyn@gmail.com>
2022-05-08 14:21:31 +0300

    delete comment

--- index.js
+++ index.js
@@ -5,8 +5,6 @@ const userRouter = require('./routers/userRouters.js');
 const answerRouter = require('./routers/answerRouter.js');
 const {URI_MONGO_DB} = require('./configs.js');
 const passfather = require('passfather');
-//const { error } = require("console");
-
 
 const PORT = process.PORT || 5000;
 
@@ -17,12 +15,10 @@ app.use(express.json());
 app.use('/user', userRouter);
 app.use('/answer', answerRouter);
 
-
 const start = async () => {
 
   try {
    
-    //await mongoose.connect('mongodb+srv://MathTest:MathTest@mathtest.2igtq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
     await mongoose.connect(URI_MONGO_DB)
       .then(() => console.log('MongoDB connect.'))
       .catch(error => console.log(error));
